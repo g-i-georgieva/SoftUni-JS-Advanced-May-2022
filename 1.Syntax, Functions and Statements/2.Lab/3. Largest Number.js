ae26870fac5fae112ce837f7c7b10e74350e0fee
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-07-03 23:06:57 +0300

    Add files via upload

---
+++ 1.Syntax, Functions and Statements/2.Lab/3. Largest Number.js
@@ -0,0 +1,7 @@
+function solve(p1, p2, p3) {
+    // let largest = Math.max(p1, p2, p3)
+    // console.log(`The largest number is ${largest}.`);
+
+    return `The largest number is ${Math.max(p1, p2, p3)}.`
+}
+console.log(solve(5, -3, 16))
