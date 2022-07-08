c8c6acce66646ecc67e4a1408b51360c9ccacf55
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-07-08 12:33:59 +0300

    Add files via upload

---
+++ 2.Arrays and Nested Arrays/2.Lab/04. Negative_Positive Numbers.js
@@ -0,0 +1,14 @@
+function solve(input) {
+    let result = []
+
+    for (let el of input) {
+        if(el>=0){
+            result.push(el)
+        }else{
+            result.unshift(el)
+        }
+    }
+
+    console.log(result.join('\n'));
+}
+solve([7, -2, 8, 9])
