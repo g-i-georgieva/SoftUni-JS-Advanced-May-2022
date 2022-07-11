4cf91c04384e4de02664f004a4bf429c21d50070
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-07-11 22:26:56 +0300

    Add files via upload

---
+++ 2.Arrays and Nested Arrays/3.Exercise/02. Print every N-th Element from an Array.js
@@ -0,0 +1,28 @@
+function printNthElement(array, num) {
+    let result = []
+    for (let i = 0; i < array.length; i+=num) {
+          result.push(array[i])      
+    }
+
+    return result
+}
+console.log(printNthElement(['5',
+    '20',
+    '31',
+    '4',
+    '20'],
+    2
+))
+console.log(printNthElement(['dsa',
+    'asd',
+    'test',
+    'tset'],
+    2
+))
+console.log(printNthElement(['1',
+    '2',
+    '3',
+    '4',
+    '5'],
+    6
+))
