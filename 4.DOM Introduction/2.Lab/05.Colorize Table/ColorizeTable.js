808ba6ae54de12ca471a87bb12b1529f9f5bd640
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-07-24 22:24:54 +0300

    Add files via upload

---
+++ 4.DOM Introduction/2.Lab/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,19 @@
+function colorize() {
+  //First solution with css selector
+  //    let rowElements = document.querySelectorAll('tr:nth-of-type(2n) td')
+
+  //    rowElements.forEach(x=>{
+  //        x.style.backgroundColor = 'teal'
+  //    })
+  //
+  //First solution with array 
+  let rowElements = document.getElementsByTagName("tr");
+  //Convert to array
+  let rows = Array.from(rowElements);
+
+  rows.forEach((x, i) => {
+    if (i % 2 !== 0) {
+      x.style.backgroundColor = "teal";
+    }
+  });
+}
