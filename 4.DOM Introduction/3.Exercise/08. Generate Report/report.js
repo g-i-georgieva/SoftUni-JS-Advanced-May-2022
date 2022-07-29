8998529f48aa594655fceeda6e1defd3dbb6435b
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-07-29 08:40:53 +0300

    Add files via upload

---
+++ 4.DOM Introduction/3.Exercise/08. Generate Report/report.js
@@ -0,0 +1,30 @@
+function generateReport() {
+    let checkedCheckboxesElement = document.querySelectorAll(
+      `thead tr th input:checked`
+    );
+    let keys = [];
+  
+    for (let checked of checkedCheckboxesElement) {
+      keys.push([checked.name, checked.parentElement.cellIndex]);
+      // console.log(checked.name);
+    }
+  
+    let data = [];
+    for (let key of keys) {
+      let values = document.querySelectorAll(
+        `tbody tr td:nth-child(${key[1] + 1})`
+      );
+      let valuesLength = values.length;
+  
+      for (let i = 0; i < valuesLength; i++) {
+        if (!data[i]) {
+          data.push({ [key[0]]: values[i].textContent });
+        } else {
+          Object.assign(data[i], { [key[0]]: values[i].textContent });
+        }
+      }
+    }
+    let outputElement = document.querySelector("textarea#output");
+    outputElement.value = JSON.stringify(data);
+    // console.log(data);
+  }
