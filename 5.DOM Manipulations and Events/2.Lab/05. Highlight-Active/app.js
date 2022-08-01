79b5b0a7cd5c379eea11fb347d046c626e9f2736
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-08-01 14:39:36 +0300

    Add files via upload

---
+++ 5.DOM Manipulations and Events/2.Lab/05. Highlight-Active/app.js
@@ -0,0 +1,14 @@
+function focused() {
+    Array.from(document.querySelectorAll("input")).forEach((i) => {
+      i.addEventListener("focus", onFocus);
+      i.addEventListener("blur", onBlur);
+    });
+  
+    function onFocus(ev) {
+      ev.target.parentElement.className = 'focused'
+    }
+  
+    function onBlur(ev) {
+      ev.target.parentElement.className = 'none'
+    }
+  }
