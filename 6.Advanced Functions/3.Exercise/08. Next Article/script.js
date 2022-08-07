4134fdf7389ef6e480f1087d3f7fb0e47308bf35
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-08-07 23:43:48 +0300

    Add files via upload

---
+++ 6.Advanced Functions/3.Exercise/08. Next Article/script.js
@@ -0,0 +1,15 @@
+function getArticleGenerator(articles) {
+  const contentElem = document.getElementById("content");
+
+  function articleCreator() {
+    let article = articles.shift();
+
+    if (article != undefined) {
+      const articleElem = document.createElement("article");
+      articleElem.textContent = article;
+      contentElem.appendChild(articleElem);
+    }
+  }
+
+  return articleCreator;
+}
