c55b29f7030700236c732db944978050e96bdda1
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-08-09 12:46:47 +0300

    Add files via upload

---
+++ 7.Unit Testing and Error Handling/2.Lab/4.sum/sum.test.js
@@ -0,0 +1,18 @@
+const { sum } = require("./sum.js");
+const { expect } = require("chai");
+
+describe('function sum tests', () => {
+    it('happy path', () => {
+        expect(sum([1,2,3,4])).to.be.equal(10)
+    });
+
+    it('happy path', () => {
+        expect(sum([1,2,'3','4'])).to.be.equal(10)
+    });
+
+    it('false output', () => {
+        expect(sum(['a',2,'3','4'])).to.deep.equal(NaN)
+        expect(sum('shdgajsh')).to.deep.equal(NaN)
+        
+    });
+});
